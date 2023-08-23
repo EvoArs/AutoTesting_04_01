3004715b53c28419cc9330ca4368b2867efb41bd
EvoArs <EvoyanAG@mail.ru>
2023-08-23 13:24:45 +0300

    add test

--- src/test/java/ru/netology/TestFormDeliveryDebitCard.java
+++ src/test/java/ru/netology/TestFormDeliveryDebitCard.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFormDeliveryDebitCard {
@@ -26,8 +30,7 @@ public class TestFormDeliveryDebitCard {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        Опция отключающаяя отображение работы браузера при запуске тестов
-        options.addArguments("--headless");
+        options.addArguments("--headless"); //        Опция отключающаяя отображение работы браузера при запуске тестов
         driver = new ChromeDriver(options);
 
     }
@@ -37,18 +40,25 @@ public class TestFormDeliveryDebitCard {
         driver.quit();
         driver = null;
     }
+
     @Test
     void testDebitCardPage() {
         //1. загрузка страницы
-        driver.get("http://localhost:7777");
+        open("http://localhost:9999");
+        //2. Поиск города по первым двум буквам
+        $("[data-test-id='city'] input").setValue("Са");
+        $$("span.menu-item__control").find(exactText("Санкт-Петербург")).click();
+
+
 
         //2. поиск и взаимодействие с элементами
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Эвоян Арсен");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79602358994");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.cssSelector("button.button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+
+//        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Эвоян Арсен");
+//        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79602358994");
+//        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+//        driver.findElement(By.cssSelector("button.button")).click();
+//        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
 //        .trim() отрезает пробелы в тексте
 //        Опция отображения работы браузера при запуске тестов
 //        Thread.sleep(5000);
