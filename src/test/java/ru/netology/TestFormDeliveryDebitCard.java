bcb1e553f2e77aec59d86f88a9fddb766970bd09
EvoArs <EvoyanAG@mail.ru>
2023-08-23 14:58:30 +0300

    add test

--- src/test/java/ru/netology/TestFormDeliveryDebitCard.java
+++ src/test/java/ru/netology/TestFormDeliveryDebitCard.java
@@ -1,20 +1,21 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Condition.exactText;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFormDeliveryDebitCard {
     private WebDriver driver;
@@ -41,26 +42,41 @@ public class TestFormDeliveryDebitCard {
         driver = null;
     }
 
+    // Универсальный метод для генерации даты
+    // где:
+    // addDay - количество дней добавляемых к текущей дате
+    // String pattern - шаблон для преобразование даты в строчный формат (ДД.ММ.ГГГГ)
+    // LocalDate.now() - метод возвращающий текущую дату
+    // plusDays(addDay) - метод плюсующий к текущей дате параметр addDay
+    // format() - метод форматирующий дату в строку
+    private String genDate(int addDay, String pattern) {
+        return LocalDate.now().plusDays(addDay).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
     @Test
     void testDebitCardPage() {
         //1. загрузка страницы
         open("http://localhost:9999");
         //2. Поиск города по первым двум буквам
         $("[data-test-id='city'] input").setValue("Са");
-        $$("span.menu-item__control").find(exactText("Санкт-Петербург")).click();
-
-
-
-        //2. поиск и взаимодействие с элементами
-
-//        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Эвоян Арсен");
-//        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79602358994");
-//        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-//        driver.findElement(By.cssSelector("button.button")).click();
-//        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
-//        .trim() отрезает пробелы в тексте
-//        Опция отображения работы браузера при запуске тестов
-//        Thread.sleep(5000);
+        $$("span.menu-item__control").findBy(text("Санкт-Петербург")).click();
+        //3. Выбор даты (передаём плюс 4 дня и формат даты)
+        String currentDate = genDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        //4. Ввод ФИО
+        $("[data-test-id='name'] input").setValue("Эвоян Арсен Гагикович");
+        //5. Ввод телефона
+        $("[data-test-id='phone'] input").setValue("+79602358994");
+        //6. Установка "галочку" согласия
+        $("[data-test-id='agreement']").click();
+        //7. Нажатие кнопки "Забронировать"
+        $("button.button").click();
+        //8. Фиксация всплывающего сообщения
+        $("div.notification__title").shouldHave(text("Успешно!"),
+                Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + currentDate),
+                Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
