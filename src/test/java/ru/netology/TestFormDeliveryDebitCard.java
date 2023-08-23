f3182333e4a6fba7e350d6d4e2250c18bf4d5cc7
EvoArs <EvoyanAG@mail.ru>
2023-08-23 15:54:47 +0300

    reformat code

--- src/test/java/ru/netology/TestFormDeliveryDebitCard.java
+++ src/test/java/ru/netology/TestFormDeliveryDebitCard.java
@@ -1,14 +1,8 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -19,27 +13,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TestFormDeliveryDebitCard {
     private WebDriver driver;
-
-    // настройка под драйвер хрома
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
 
     // Универсальный метод для генерации даты
     // где:
